813eeedf25b0eb016084883a3857b73a5e130962
philipp012 <philipp.kiss@bluewin.ch>
2019-11-26 18:57:11 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        play();
+    }
+
+    private static void play() {
+        Board board = new Board();
+        board.setUp();
+    }
+}
