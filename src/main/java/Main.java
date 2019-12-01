25f5275215db558258eb0a19c640898a6378b5f1
philipp012 <philipp.kiss@bluewin.ch>
2019-12-01 13:06:45 +0100

    added player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,45 @@
+import Enums.COLOR;
+
 public class Main {
     public static void main(String[] args) {
         play();
     }
 
     private static void play() {
+        System.out.println("Console Chess");
+        System.out.println("=============");
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        System.out.print("Name of Player 1: ");
+        player1.setName(player1.getInput());
+        while (player1.getColor() == null) {
+            String color = player1.getInput().trim().toLowerCase();
+            System.out.print("Do you want to play as white or as black <w/b> ");
+            if (color.equals("w")) {
+                player1.setColor(COLOR.WHITE);
+                player2.setColor(COLOR.BLACK);
+            } else if (color.equals("b")) {
+                player1.setColor(COLOR.BLACK);
+                player2.setColor(COLOR.WHITE);
+            } else {
+                System.out.println("Please pick a w or b");
+            }
+        }
+
+        System.out.print("Name of Player 2: ");
+        player2.setName(player2.getInput());
+
         Board board = new Board();
         board.setUp();
-        board.printBoard();
+        int moves = 0;
+        while (true) {
+            if (player1.getColor().equals(COLOR.WHITE)) {
+                player1.getMove();
+            } else {
+            }
+        }
     }
+
 }
---
+++ src/main/java/Player.java
@@ -0,0 +1,89 @@
+import Enums.COLOR;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Player {
+    private COLOR color;
+    private String name;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final char LETTERS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',};
+
+    public COLOR getColor() {
+        return color;
+    }
+
+    public void setColor(COLOR color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInput() {
+        String input =  scanner.nextLine();
+        scanner.close();
+        return input;
+    }
+
+    public int[][] getMove() {
+        boolean valid = false;
+        int[][] move = new int[1][1];
+        int[] from = new int[2];
+        int[] to = new int[2];
+        while (!valid) {
+            System.out.print(this.color + "'s turn: ");
+            String input = getInput();
+            if (checkInputValidity(input)) {
+                valid = true;
+                from[0] = input.charAt(0);
+                from[1] = input.charAt(1);
+                to[0] = input.charAt(3);
+                to[1] = input.charAt(4);
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+        
+    }
+
+    private boolean checkInputValidity(String input) {
+        if (input.length() != 5) {
+            return false;
+        }
+        boolean valid = false;
+        for (char c : LETTERS) {
+            if (c == input.charAt(0)) {
+                valid = true;
+                break;
+            }
+        }
+        if (!valid) {
+            return false;
+        }
+        valid = false;
+        for (char c : LETTERS) {
+            if (c == input.charAt(3)) {
+                valid = true;
+                break;
+            }
+        }
+        if (!valid) {
+            return false;
+        }
+        int intFrom = Integer.parseInt(String.valueOf(input.charAt(1)));
+        int intTo = Integer.parseInt(String.valueOf(input.charAt(4)));
+        if (0 > intFrom || intFrom > 9 | 0 > intTo || intTo > 9) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
